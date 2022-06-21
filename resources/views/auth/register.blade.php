f8ce4cccef8ac3504ba2e405950f494152749ff0
Michael Saiba <michaelsaiba84@gmail.com>
2022-06-21 11:09:39 +0300

    added email template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-            @include('auth.partials.logo')
+            {{-- @include('auth.partials.logo') --}}
 
             <div class="card">
                 <div class="card-header">
